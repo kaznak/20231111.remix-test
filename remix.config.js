c68f521889b551e1a57b60b0476a3a98bffc250f
Nakamura Kazutaka <kaznak@ipebble.org>
2023-11-12 16:22:09 +0900

    configure for server side rendering

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,18 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  serverDependenciesToBundle: [
+    "@apollo/client",
+    "tslib",
+    "ts-invariant",
+    "@wry/trie",
+    "zen-observable-ts",
+    "symbol-observable",
+    "@wry/equality",
+    "optimism",
+    "@wry/context",
+    "graphql-tag",
+    "@apollo/client/react/ssr",
+  ],
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
